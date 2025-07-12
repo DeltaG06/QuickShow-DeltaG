6da7b8483a14bb6cb0a7ad92c035b77e7160a7eb
Gaurav Parker <gauravparker955@gmail.com>
2025-07-12 20:40:55 +0530

    added paths and rotes dummy

--- appclone/Movie_website/client/src/App.jsx
+++ appclone/Movie_website/client/src/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import {Route, Routes} from 'react-router-dom'
+import Movies from './pages/Movies'
+import SeatLayout from './pages/SeatLayout'
+import MovieDetails from './pages/MovieDetails'
+import Home from './pages/home'
 
 const App = () => {
   return (
-    <div>
+    <>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/movies' element={<Movies/>}/>
+        <Route path='/movies/:id' element={<MovieDetails/>}/>
+        <Route path='/movies/:id/:date' element={<SeatLayout/>}/>
+      </Routes>
+
       
-    </div>
+    </>
   )
 }
 
